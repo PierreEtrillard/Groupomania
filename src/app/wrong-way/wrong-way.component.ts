fdfaa7c6e6a2565176d8cc1502a202290bfb10ba
Pierre Etrillard <pierre.etrillard@live.fr>
2022-10-03 18:12:04 +0200

    ajout des fichiers de models et services dans laurs dossiers respectifs

---
+++ src/app/wrong-way/wrong-way.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wrong-way',
+  templateUrl: './wrong-way.component.html',
+  styleUrls: ['./wrong-way.component.sass']
+})
+export class WrongWayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
