2ed61219319770ada84193e469b0730793a28ada
Pierre Etrillard <pierre.etrillard@live.fr>
2022-10-12 15:56:55 +0200

    implementation du login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginForm:FormGroup
+logData:any
+  constructor(private formBuilder:FormBuilder, private userService:UsersService) { }
 
   ngOnInit(): void {
+    this.loginForm =this.formBuilder.group({
+      pseudo: ["",[Validators.required,Validators.minLength(2)]],
+      //prévoir une auth avec mail => email: ["",[Validators.required,Validators.email] ],
+      password: ["",[Validators.required,Validators.minLength(9)]]
+    })
   }
-
+login(){
+  this.logData={
+    pseudo:this.loginForm.value.pseudo,
+    password:this.loginForm.value.password
+  }
+this.userService.login(this.logData).subscribe()
+}
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -12,11 +12,21 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   newUser(user: User): Observable<User> {
-    return this.http.post<User>(`${apiUrl}/signin`,user)
+    return this.http.post<User>(`${apiUrl}/auth/signin`,user)
   }
   updateUser(user: User,id:string): Observable<User> {
     console.log(`Mise à jour de l'utilisateur:  ${JSON.stringify(user)} envoyé à ${apiUrl}/signin/:${id}`);
-    return this.http.patch<User>(`${apiUrl}/signin/:${id}`,user).pipe(
+    return this.http.patch<User>(`${apiUrl}/auth/signin/:${id}`,user).pipe(
+      tap(
+        (res) => console.log(res)
+      ),
+      catchError((err) => {
+        console.error(err)
+        return of()
+      }))
+  }
+  login(logData:any):Observable<any>{
+    return this.http.post<any>(`${apiUrl}/auth/login`,logData).pipe(
       tap(
         (res) => console.log(res)
       ),
